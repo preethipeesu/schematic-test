8fc1cdcb982c1d20277328b438ba95a0c4c102b1
preethipeesu <ppeesu@ctepl.com>
2023-05-02 19:17:13 +0530

    1stMsg

---
+++ projects/super-ui-lib/src/lib/super-ui-lib.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { SuperUiLibComponent } from './super-ui-lib.component';
+
+@NgModule({
+  declarations: [
+    SuperUiLibComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    SuperUiLibComponent
+  ]
+})
+
+export class SuperUiLibModule { }
